dc3bf30d35b328334d69b677c0482370977db9f9
k-davis <kaseydavis216@gmail.com>
2019-01-26 14:08:52 -0800

    Added function headers

---
+++ projs/p1/yahtzee.java
@@ -0,0 +1,36 @@
+public class Yahtzee1{
+	public static void main(String[] args){
+		final int DICE_IN_PLAY = 5;
+		int hand = new int[DICE_IN_PLAY];
+
+		char playAgain = 'y';
+
+		while(playAgain == 'y'){
+
+		}
+	}
+
+	int rollDie(){
+		return 1;
+	}
+
+	int maxOfAKindFound(int hand[]){
+		return 1;
+	}
+
+	int totalAllDice(int hand[]){
+		return 1;
+	}
+
+	void sortArray(int array[], int size){
+
+	}
+
+	int maxStraightFound(int hand[]){
+		return 1;
+	}
+
+	boolean fullHouseFound(int hand[]){
+		return true;
+	}
+}
